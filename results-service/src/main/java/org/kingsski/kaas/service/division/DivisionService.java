78c7a9efc7afa30f7fbabeb2dfa5c60cf47e7dd6
andrewflbarnes <andrew.barnes2@mastercard.com>
2019-07-25 19:27:35 +0100

    Add division controller, service and config

---
+++ results-service/src/main/java/org/kingsski/kaas/service/division/DivisionService.java
@@ -0,0 +1,61 @@
+package org.kingsski.kaas.service.division;
+
+import org.kingsski.kaas.database.division.Division;
+import org.kingsski.kaas.database.division.DivisionDao;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * This service defines and implements operations for {@link Division}s.
+ * Any APIs, resources or other services wanting to service or
+ * mutate {@link Division}s should do so through this service and
+ * not through other means (e.g. more directly through DAOs)
+ */
+@Service
+public class DivisionService {
+
+    private DivisionDao divisionDao;
+
+    public DivisionService(DivisionDao divisionDao) {
+        this.divisionDao = divisionDao;
+    }
+
+    /**
+     * Get all {@link Division}s
+     *
+     * @return a {@link List} of {@link Division}s
+     */
+    public List<Division> getDivisions() {
+        return divisionDao.getDivisions();
+    }
+
+    /**
+     * Get all {@link Division}s by competition
+     *
+     * @return a {@link List} of {@link Division}s
+     */
+    public List<Division> getDivisionsByCompetition(String competition) {
+        return divisionDao.getDivisionsByCompetition(competition);
+    }
+
+    /**
+     * Get a {@link Division} by name
+     *
+     * @param name the name of the division to get
+     * @return a {@link Division}
+     */
+    public Division getDivisionByName(String name) {
+        return divisionDao.getDivisionByName(name);
+    }
+
+    /**
+     * Get a {@link Division} by id
+     *
+     * @param id the id of the division to get
+     * @return a {@link Division}
+     */
+    public Division getDivisionById(long id) {
+        return divisionDao.getDivisionById(id);
+    }
+}
